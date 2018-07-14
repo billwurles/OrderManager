d52677e4a3e45fb0341c43b444e445b16e6c9567
kelechi96 <kelechi8192@gmail.com>
2018-07-14 09:36:12 +0100

    Separating multi-class files

---
+++ test/MockOM.java
@@ -0,0 +1,22 @@
+class MockOM extends Thread{
+	InetSocketAddress[] clients;
+	InetSocketAddress[] routers;
+	InetSocketAddress trader;
+	LiveMarketData liveMarketData;
+	MockOM(String name,InetSocketAddress[] routers,InetSocketAddress[] clients,InetSocketAddress trader,LiveMarketData liveMarketData){
+		this.clients=clients;
+		this.routers=routers;
+		this.trader=trader;
+		this.liveMarketData=liveMarketData;
+		this.setName(name);
+	}
+	@Override
+	public void run(){
+		try{
+			//In order to debug constructors you can do F5 F7 F5
+			new OrderManager(routers,clients,trader,liveMarketData);
+		}catch(IOException | ClassNotFoundException | InterruptedException ex){
+			Logger.getLogger(MockOM.class.getName()).log(Level.SEVERE,null,ex);
+		}
+	}
+}
