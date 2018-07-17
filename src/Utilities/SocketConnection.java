00c772019552553435158dc3fe7bc2adc1ae1967
billwurles <burlesw@gmail.com>
2018-07-17 11:02:24 +0100

    SocketConnection class complete

--- src/Utilities/SocketConnection.java
+++ src/Utilities/SocketConnection.java
@@ -1,34 +1,50 @@
 package Utilities;
 
 import java.io.IOException;
-import java.io.ObjectOutputStream;
 import java.net.InetSocketAddress;
 import java.net.Socket;
+import java.net.SocketAddress;
+import java.nio.ByteBuffer;
+import java.nio.channels.SelectionKey;
+import java.nio.channels.Selector;
+import java.nio.channels.ServerSocketChannel;
 import java.nio.channels.SocketChannel;
+import java.util.*;
 
 /**
  * Created by alumniCurie16 on 17/07/2018.
  */
 public class SocketConnection {
 
+    SocketChannel channel;
+    ServerSocketChannel server;
     Socket socket;
-    InetSocketAddress location;
+    InetSocketAddress address;
+    Selector selector;
+    Iterator keys;
+
+    ByteBuffer buffer;
+    Map<SocketChannel, List> dataMap;
+
     boolean connected;
 
+
+
     public SocketConnection(InetSocketAddress location) throws InterruptedException {
-        this.location=location;
+        this.address=location;
         int tryCounter = 0;
         connected=false;
         while(!connected&&tryCounter<600) {
             try {
-                socket = new Socket(location.getHostName(), location.getPort());
-                socket.setKeepAlive(true);
+                channel = SocketChannel.open(location);
+                System.err.printf("Connection %s to %s:%s made\n",Thread.currentThread().getName(),location.getHostName(),location.getPort());
                 connected=true;
             } catch (IOException e) {
                 Thread.sleep(500);
                 tryCounter++;
             }
         }
+        dataMap = new HashMap<>();
     }
 
     private Socket connect(InetSocketAddress location) throws InterruptedException{
@@ -48,11 +64,78 @@ public class SocketConnection {
         return null;
     }
 
-    private void sendMessage(){
-        ObjectOutputStream ost=new ObjectOutputStream(trader.getOutputStream());
-        ost.writeObject(method);
-        ost.writeInt(id);
-        ost.writeObject(o);
-        ost.flush();
+    private void sendMessage(String message) throws IOException {
+        byte[] data = message.getBytes();
+        buffer = ByteBuffer.wrap(data);
+        channel.write(buffer);
+        System.err.printf("Conn. %s sent data to %s:%s\n",Thread.currentThread().getName(),address.getHostName(),address.getPort());
+        buffer.clear();
+        //sleep(5000);
+    }
+
+    private String listenForMessage() throws IOException {
+        this.selector = Selector.open();
+        server = ServerSocketChannel.open();
+        server.configureBlocking(false); //TODO maybe we want this to be true?
+
+        server.socket().bind(address);
+        server.register(selector, SelectionKey.OP_ACCEPT);
+
+        while(true){
+            System.err.printf("Conn. %s listening on %s:%s\n",Thread.currentThread().getName(),address.getHostName(),address.getPort());
+            this.selector.select();
+
+            keys = this.selector.selectedKeys().iterator();
+            while(keys.hasNext()){
+                SelectionKey key = (SelectionKey) keys.next();
+                keys.remove();
+                if(!key.isValid()){
+                    continue;
+                } else if(key.isAcceptable()){
+                    accept(key);
+                }
+                if(key.isReadable()){
+                    read(key);
+                }
+
+            }
+
+            System.err.printf("Conn. %s recieved data from %s:%s\n",Thread.currentThread().getName(),address.getHostName(),address.getPort());
+        }
+    }
+
+    private void accept(SelectionKey key) throws IOException {
+        server = (ServerSocketChannel) key.channel();
+        channel = server.accept();
+        channel.configureBlocking(false);
+        socket = channel.socket();
+
+        SocketAddress sockAddr = socket.getRemoteSocketAddress();
+        System.err.printf("Connected to: %s",sockAddr);
+
+        dataMap.put(channel,new ArrayList());
+        channel.register(selector, SelectionKey.OP_READ);
+    }
+
+    private String read(SelectionKey key) throws IOException{
+        channel = (SocketChannel) key.channel();
+        buffer = ByteBuffer.allocate(2048);
+        int numRead = -1;
+        numRead = channel.read(buffer);
+
+        if(numRead == -1){
+            dataMap.remove(channel);
+            socket = channel.socket();
+            SocketAddress sockAddr = socket.getRemoteSocketAddress();
+            System.err.printf("Conn. %s closed by %s\n",Thread.currentThread().getName(),sockAddr);
+            channel.close();
+            key.cancel();
+            return "";
+        }
+
+        String data = new String(buffer.array());
+        System.err.printf("Conn. %s recieved data from %s:%s\n%s",Thread.currentThread().getName(),address.getHostName(),address.getPort(),data);
+
+        return data;
     }
 }
